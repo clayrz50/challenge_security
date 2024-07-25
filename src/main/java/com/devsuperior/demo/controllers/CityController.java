f11024fed44a84b992d49ecef9c3e3c411c892c1
Clayton <clayrz50@gmail.com>
2024-07-25 08:22:42 -0300

    challenge completed and tested

--- src/main/java/com/devsuperior/demo/controllers/CityController.java
+++ src/main/java/com/devsuperior/demo/controllers/CityController.java
@@ -1,13 +1,11 @@
 package com.devsuperior.demo.controllers;
 
 import java.net.URI;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -18,20 +16,25 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import com.devsuperior.demo.dto.CityDTO;
 import com.devsuperior.demo.services.CityService;
 
+import jakarta.validation.Valid;
+
 @RestController
 @RequestMapping(value= "/cities")
 public class CityController {
 	@Autowired
 	private CityService service;
+	
 	@GetMapping
-	public ResponseEntity<Page<CityDTO>> findAll(Pageable pageable){
-		PageRequest pageRequest=PageRequest.of(pageable.getPageNumber(), pageable.getPageSize(),Sort.by("name"));
-		Page<CityDTO> list=service.findAll(pageRequest);
+	public ResponseEntity<List<CityDTO>> findAll() {
+		List<CityDTO> list = service.findAll();		
 		return ResponseEntity.ok().body(list);
 	}
 	
+	
+	
+	@PreAuthorize("hasRole('ROLE_ADMIN')")
 	@PostMapping
-	public ResponseEntity<CityDTO> insert(@RequestBody CityDTO dto){
+	public ResponseEntity<CityDTO> insert(@Valid @RequestBody CityDTO dto){
 		dto=service.insert(dto);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
 				.buildAndExpand(dto.getId()).toUri();
--- src/main/java/com/devsuperior/demo/services/CityService.java
+++ src/main/java/com/devsuperior/demo/services/CityService.java
@@ -1,8 +1,9 @@
 package com.devsuperior.demo.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -16,9 +17,9 @@ public class CityService {
 	private CityRepository cityRepository;
 	
 	@Transactional(readOnly = true)
-	public Page<CityDTO> findAll(Pageable pageable){
-		Page<City> page=cityRepository.findAll(pageable);
-		return page.map(x->new CityDTO(x));
+	public List<CityDTO> findAll() {
+		List<City> list = cityRepository.findAll(Sort.by("name"));
+		return list.stream().map(x -> new CityDTO(x)).toList();
 	}
 	
 	@Transactional
